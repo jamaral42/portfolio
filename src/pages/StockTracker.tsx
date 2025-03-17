cf47c399e69733e9b5b51d4886ceab14db7e330a
João Amaral <joaomramaral42@gmail.com>
2025-03-17 10:44:34 +0000

    minor fix

--- src/pages/StockTracker.tsx
+++ src/pages/StockTracker.tsx
@@ -2,31 +2,18 @@
 import React, { useState } from "react";
 import Header from "../components/stocks/Header";
 import LoginPopup from "../components/stocks/LoginPopup";
-import AwaitLogin from "../components/stocks/AwaitLogin";
 
 const StockTracker: React.FC = () => {
   const [showLogin, setShowLogin] = useState(false);
-  const [user, setUser] = useState<{ username: string } | null>(null);
 
   return (
     <div className="h-screen flex flex-col bg-gray-100">
       {showLogin && <LoginPopup onClose={() => setShowLogin(false)} />}
 
       <Header />
-      {/* Display user info if logged in */}
-      <div className="p-4">
-        {user ? <p>Welcome, {user.username}</p> : <AwaitLogin />}
-      </div>
 
-      {/* Show login button if user is not logged in */}
-      {!user && (
-        <button
-          onClick={() => setShowLogin(true)}
-          className="bg-blue-500 text-white p-2 rounded mt-4 self-center"
-        >
-          Login
-        </button>
-      )}
+
+
     </div>
   );
 };
