d9fa9af726c09dfca85fa58619193811936d74c3
João Amaral <joaomramaral42@gmail.com>
2025-07-10 12:10:55 +0100

    deleted not used code

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ import CookieBanner from './components/portfolio/CookiesBanner';
 const Portfolio = lazy(() => import('./pages/Portfolio'));
 const CookiePolicy = lazy(() => import('./pages/CookiePolicy'));
 const Main = lazy(() => import('./pages/Main'));
-const StockTracker = lazy(() => import('./pages/StockTracker'));
-const Wordle = lazy(() => import('./components/wordle/Wordle'));
 
 const App: FC = () => (
   <I18nextProvider i18n={i18n}>
@@ -19,8 +17,6 @@ const App: FC = () => (
         <Route path="*" element={<Portfolio />} />
         <Route path="/cookie-policy" element={<CookiePolicy />} />
         <Route path="/main" element={<Main />} />
-        <Route path="/wordle" element={<Wordle />} />
-        <Route path="/stock-tracker" element={<StockTracker />} />
       </Routes>
       <CookieBanner />
     </BrowserRouter>
